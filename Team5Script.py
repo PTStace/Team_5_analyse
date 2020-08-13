cd66995f79e44647c1831dafc0b2e8f71cb1879e
PTStace <68692872+PTStace@users.noreply.github.com>
2020-08-13 10:15:23 +0200

    Update Team5Script.py

--- Team5Script.py
+++ Team5Script.py
@@ -22,23 +22,20 @@ def five_num_summary(items):
     return dictionary
 
 
-def date_parser(dates):  
-    """ This function takes as input a list of these datetime strings,
-    each string formatted as 'yyyy-mm-dd hh:mm:ss' 
-    and returns only the date in 'yyyy-mm-dd' format.
+ and returns only the date in 'yyyy-mm-dd' format.
     Parameters:
     >
     items: (list), list of datetime strings.
     Returns:
     >
     returns a list of strings where each element in the
-    returned list contains only the date in the 'yyyy-mm-dd' format.""" 
-    date_only =[]  '# An empty date list (to add sublists in it in the for loop)'
-    for date in dates:  '# Setting up the condition'
-        my_list = date[: len(date) - 9]    
-        date_only.append(my_list)  '# Creating a sublist from my_list'
-        date_only[:]                     
-    '# Return the date only in 'yyyy-mm-dd' format'
+    returned list contains only the date in the 'yyyy-mm-dd' format."""
+    date_only = []  # empty date list (to add sublists in it in the for loop)
+    for date in dates:  # Setting up the condition
+        my_list = date[: len(date) - 9]
+        date_only.append(my_list)  # Creating a sublist from my_list
+        date_only[:]
+    # Return the date only in 'yyyy-mm-dd' format'
     return date_only
 
 
